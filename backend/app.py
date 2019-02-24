88b1596ab0375b3bef021af963347df08947e2d1
indionapolis <panddromas@gmail.com>
2019-02-24 18:04:09 +0300

    added division algorithm

--- backend/app.py
+++ backend/app.py
@@ -1,9 +1,8 @@
-import json
-
 from flask import Flask, request, Response, redirect, send_from_directory
-import pandas as pd
-import os
 from flask_cors import CORS
+import pandas as pd
+import json
+import os
 
 UPLOAD_FOLDER = './uploads'
 WORKING_FOLDER = './working'
@@ -72,13 +71,44 @@ def divide_into_groups():
 
         df = pd.read_excel(f'{WORKING_FOLDER}/{file}')
         df['project'] = ['' for i in range(len(df))]
+        df['team'] = ['' for i in range(len(df))]
 
-        # TODO algorithm
-        # for project in projects:
-        #     for item in df:
-        #         item
-        #     for team in project['teams']:
-        #         print(team["skills"])
+        for project in projects:
+            for team in project['teams']:
+                team_skills_set = set(team["skills"])
+                team['members'] = []
+
+                # search for people for team
+                while len(team_skills_set):
+
+                    # search for best people for team
+                    best_person = None
+                    best_person_id = 0
+                    best_intersection = 0
+                    for i, person in enumerate(df.values.tolist()):
+                        print(person)
+
+                        # if person is free
+                        if person[3] == '':
+                            person_skills_set = set(person[1].split(', ') + person[2].split(', '))
+
+                            # looking for person with biggest skill intersection with project
+                            intersection_len = len(person_skills_set.intersection(team_skills_set))
+                            if intersection_len > best_intersection:
+                                best_intersection = intersection_len
+                                best_person = person
+                                best_person_id = i
+
+                    # add best_person to the team
+                    if best_person:
+                        df['project'][best_person_id] = project['name']
+                        df['team'][best_person_id] = team['name']
+                        best_person_skills_set = set(best_person[1].split(', ') + best_person[2].split(', '))
+                        team_skills_set = team_skills_set.difference(best_person_skills_set)
+                        team['members'].append(best_person)
+                    else:
+                        # if we did not find more people for project
+                        break
 
         df.to_excel(f'{WORKING_FOLDER}/{file}', index=False)
 
@@ -113,5 +143,5 @@ def drop_all_files():
 
 
 if __name__ == '__main__':
-    drop_all_files()
+    # drop_all_files()
     app.run(debug=False, host='0.0.0.0', port=5000)
