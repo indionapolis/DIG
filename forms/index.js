1ea15f4ab738ce41df9dd6c4cd0fb05b86f492cf
Nagellan <31626879+Nagellan@users.noreply.github.com>
2019-03-28 02:54:03 +0300

    Clean the code, minor functionality * Added documentation and comments; * Changed variable names on more proper ones; * Added autofocus on input field after adding a block; * Added block save by pressing 'enter' key at input field; + Added function getGrandParent().

--- forms/index.js
+++ forms/index.js
@@ -1,5 +1,8 @@
+/**
+ * Add new block with input field for project name and 'save' and 'delete' buttons.
+ */
 function addBlock() {
-    if (document.getElementsByClassName('empty').length > 0) {
+    if (document.getElementsByClassName('empty').length > 0) {  // handle too much empty projects problem
         alert("You already have an empty project! Create it first before making a new one.");
         return;
     }
@@ -10,18 +13,32 @@ function addBlock() {
         blocksWrapper = document.getElementById("add-block").parentElement;
     var block = createBlockFromHtml('div', 'block empty', blockHtml);
     blocksWrapper.appendChild(block);
+    
+    var projectNameInput = block.getElementsByTagName('input')[0];
+    projectNameInput.focus();    // focus on newly added block's input field
+
+    projectNameInput.onkeydown = function(e) {   // save the project name by pressing 'enter' key
+        if (e.keyCode == 13)
+            saveBlock(block.getElementsByClassName('save')[0]);
+    }
 }
 
+/**
+ * Complete block creation by saving it.
+ * @param {*} saveBtn save button needed for getting the block itself.
+ */
 function saveBlock(saveBtn) {
-    block = saveBtn.parentElement.parentElement;
-    var title = block.firstChild.firstChild.value;
-    if (title.replace(/\s/g, '') == "") {
+    block = saveBtn.getGrandParent(saveBtn);
+    var projectNameInput = block.getElementsByTagName('input')[0];
+    var title = projectNameInput.value;
+    
+    if (title.replace(/\s/g, '') == "") {   // handle empty project name problem
         alert("Your project name is empty! Fill in something.");
-        block.firstChild.firstChild.value = "";
+        projectNameInput.value = "";
         return;
     }
     
-    block.firstChild.firstChild.remove();
+    projectNameInput.remove();
     block.firstChild.textContent += title;
 
     block.classList.remove('empty');
@@ -30,15 +47,30 @@ function saveBlock(saveBtn) {
     saveBtn.parentElement.innerHTML = toolsHtml.trim();
 }
 
+/**
+ * Delete given block's grandparent.
+ * @param {*} deleteBtn delete button neede for getting its grandparent and deleting it.
+ */
 function deleteBlock(deleteBtn) {
-    block = deleteBtn.parentElement.parentElement;
+    block = deleteBtn.getGrandParent(deleteBtn);
     block.remove();
 }
 
+/**
+ * Create block from html string.
+ * @param {*} type Type of block.
+ * @param {*} elemClass Block's class.
+ * @param {*} html Html code for block.
+ */
 function createBlockFromHtml(type, elemClass, html) {
     var block = document.createElement(type);
     block.innerHTML = html.trim();
     block.classList = elemClass;
 
     return block;
+}
+
+// get block's grandparent
+function getGrandParent(elem) {
+    return elem.parentElement.parentElement;
 }
