824520b52a189a3f1adb5531723f01e40132db1f
indionapolis <panddromas@gmail.com>
2019-04-09 20:53:37 +0300

    separeted division algorithm and added /projects endpoint

--- backend/app.py
+++ backend/app.py
@@ -5,6 +5,8 @@ import json
 import os
 import uuid
 
+from division_core import divide
+
 UPLOAD_FOLDER = './uploads'
 WORKING_FOLDER = './working'
 RESULT_FOLDER = './results'
@@ -31,7 +33,6 @@ def allowed_file(filename):
 
 @app.route('/upload', methods=['POST'])
 def upload_file():
-
     file = request.files['file']
 
     if file and allowed_file(file.filename):
@@ -66,94 +67,12 @@ def divide_into_groups():
         uid = request.headers['uuid']
         file = FILES_MAP[uid]
 
-        projects = json.loads(request.data)
-        df = pd.read_excel(f'{WORKING_FOLDER}/{file}')
-        # to send JSON response
-        people = df.to_dict('records')
-        df['project'] = ['' for i in range(len(df))]
-        df['team'] = ['' for i in range(len(df))]
+        configuration = json.loads(request.data)
 
-        soft_skills = set(map(lambda x: x.lower(), open('./dataset/soft_skills.txt').read().split('\n')))
-
-        for project in projects:
-            for team in project['teams']:
-                team_skills_set = set(map(lambda x: x.lower(), team["skills"]))
-                team['members'] = []
-                # in case team do not need soft skills
-                skill_flag = len(soft_skills.intersection(team_skills_set))
-
-                # search for people for team
-                while len(team_skills_set):
-
-                    # search for best people for team
-                    best_person = None
-                    best_person_id = 0
-                    best_intersection = 0
-                    for i, person in enumerate(df.to_dict('records')):
-
-                        # if person is free
-                        if person['project'] == '':
-                            person_hard_skills_set = set(map(lambda x: x.lower(), person['Hard Skills'].split(', ')))
-                            person_soft_skills_set = set(map(lambda x: x.lower(), person['Soft Skills'].split(', ')))
-
-                            # looking for person with biggest skill intersection with project
-                            hard_intersection_len = len(person_hard_skills_set.intersection(team_skills_set))
-                            soft_intersection_len = len(person_soft_skills_set.intersection(team_skills_set))
-                            if hard_intersection_len > best_intersection and (soft_intersection_len or not skill_flag):
-                                best_intersection = hard_intersection_len
-                                best_person = person
-                                best_person_id = i
-
-                    # add best_person to the team
-                    if best_person:
-                        df['project'][best_person_id] = project['name']
-                        df['team'][best_person_id] = team['name']
-                        best_person_skills_set = set(best_person['Hard Skills'].split(', '))
-                        team_skills_set = team_skills_set.difference(best_person_skills_set)
-                        team['members'].append(people[best_person_id])
-                    else:
-                        # if we did not find more people for project
-                        break
-
-        for project in projects:
-            for team in project['teams']:
-                team_skills_set = set(map(lambda x: x.lower(), team["skills"]))
-                # in case team do not need soft skills
-                skill_flag = len(soft_skills.intersection(team_skills_set))
-
-                while len(team['members']) != team['size']:
-                    # search for best people for team
-                    best_person = None
-                    best_person_id = 0
-                    best_intersection = 0
-                    for i, person in enumerate(df.to_dict('records')):
-
-                        # if person is free
-                        if person['project'] == '':
-                            person_hard_skills_set = set(map(lambda x: x.lower(), person['Hard Skills'].split(', ')))
-                            person_soft_skills_set = set(map(lambda x: x.lower(), person['Soft Skills'].split(', ')))
-
-                            # looking for person with biggest skill intersection with project
-                            hard_intersection_len = len(person_hard_skills_set.intersection(team_skills_set))
-                            soft_intersection_len = len(person_soft_skills_set.intersection(team_skills_set))
-                            if hard_intersection_len > best_intersection and (soft_intersection_len or not skill_flag):
-                                best_intersection = hard_intersection_len
-                                best_person = person
-                                best_person_id = i
-
-                    # add best_person to the team
-                    if best_person:
-                        df['project'][best_person_id] = project['name']
-                        df['team'][best_person_id] = team['name']
-                        team['members'].append(people[best_person_id])
-                    else:
-                        # if we did not find more people for project
-                        break
-
-        df.to_excel(f'{WORKING_FOLDER}/{file}', index=False)
+        divide(configuration, file)
 
         os.rename(f'{WORKING_FOLDER}/{file}', f'{RESULT_FOLDER}/{file}')
-        return Response(json.dumps(projects), status=200)
+        return Response(json.dumps(configuration), status=200)
     except KeyError:
         return Response(status=401)
 
@@ -168,6 +87,11 @@ def download():
         return send_from_directory(DELETE_FOLDER, file)
     except KeyError:
         return Response(status=401)
+
+
+@app.route('/projects', methods=['GET', 'POST'])
+def projects():
+    pass
 
 
 @app.route('/skill_suggestion', methods=['GET'])
@@ -199,5 +123,6 @@ def drop_all_files():
 
 if __name__ == '__main__':
     drop_all_files()
-    SKILL_SET = open('dataset/soft_skills.txt', 'r').read().split('\n') + open('dataset/hard_skills.txt', 'r').read().split('\n')
+    SKILL_SET = open('dataset/soft_skills.txt', 'r').read().split('\n') + open('dataset/hard_skills.txt',
+                                                                               'r').read().split('\n')
     app.run(debug=False, host='0.0.0.0', port=5000)
---
+++ backend/division_core.py
@@ -0,0 +1,89 @@
+import pandas as pd
+import app
+
+
+def divide(configuration, file):
+    df = pd.read_excel(f'{app.WORKING_FOLDER}/{file}')
+    # to send JSON response
+    people = df.to_dict('records')
+    df['project'] = ['' for i in range(len(df))]
+    df['team'] = ['' for i in range(len(df))]
+
+    soft_skills = set(map(lambda x: x.lower(), open('./dataset/soft_skills.txt').read().split('\n')))
+
+    for project in configuration:
+        for team in project['teams']:
+            team_skills_set = set(map(lambda x: x.lower(), team["skills"]))
+            team['members'] = []
+            # in case team do not need soft skills
+            skill_flag = len(soft_skills.intersection(team_skills_set))
+
+            # search for people for team
+            while len(team_skills_set):
+
+                # search for best people for team
+                best_person = None
+                best_person_id = 0
+                best_intersection = 0
+                for i, person in enumerate(df.to_dict('records')):
+
+                    # if person is free
+                    if person['project'] == '':
+                        person_hard_skills_set = set(map(lambda x: x.lower(), person['Hard Skills'].split(', ')))
+                        person_soft_skills_set = set(map(lambda x: x.lower(), person['Soft Skills'].split(', ')))
+
+                        # looking for person with biggest skill intersection with project
+                        hard_intersection_len = len(person_hard_skills_set.intersection(team_skills_set))
+                        soft_intersection_len = len(person_soft_skills_set.intersection(team_skills_set))
+                        if hard_intersection_len > best_intersection and (soft_intersection_len or not skill_flag):
+                            best_intersection = hard_intersection_len
+                            best_person = person
+                            best_person_id = i
+
+                # add best_person to the team
+                if best_person:
+                    df['project'][best_person_id] = project['name']
+                    df['team'][best_person_id] = team['name']
+                    best_person_skills_set = set(best_person['Hard Skills'].split(', '))
+                    team_skills_set = team_skills_set.difference(best_person_skills_set)
+                    team['members'].append(people[best_person_id])
+                else:
+                    # if we did not find more people for project
+                    break
+
+    for project in configuration:
+        for team in project['teams']:
+            team_skills_set = set(map(lambda x: x.lower(), team["skills"]))
+            # in case team do not need soft skills
+            skill_flag = len(soft_skills.intersection(team_skills_set))
+
+            while len(team['members']) != team['size']:
+                # search for best people for team
+                best_person = None
+                best_person_id = 0
+                best_intersection = 0
+                for i, person in enumerate(df.to_dict('records')):
+
+                    # if person is free
+                    if person['project'] == '':
+                        person_hard_skills_set = set(map(lambda x: x.lower(), person['Hard Skills'].split(', ')))
+                        person_soft_skills_set = set(map(lambda x: x.lower(), person['Soft Skills'].split(', ')))
+
+                        # looking for person with biggest skill intersection with project
+                        hard_intersection_len = len(person_hard_skills_set.intersection(team_skills_set))
+                        soft_intersection_len = len(person_soft_skills_set.intersection(team_skills_set))
+                        if hard_intersection_len > best_intersection and (soft_intersection_len or not skill_flag):
+                            best_intersection = hard_intersection_len
+                            best_person = person
+                            best_person_id = i
+
+                # add best_person to the team
+                if best_person:
+                    df['project'][best_person_id] = project['name']
+                    df['team'][best_person_id] = team['name']
+                    team['members'].append(people[best_person_id])
+                else:
+                    # if we did not find more people for project
+                    break
+
+    df.to_excel(f'{app.WORKING_FOLDER}/{file}', index=False)
